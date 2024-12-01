7af05ac3291734d5e81623d9d7fce5d5eb07ee42
n0bis <supermads@hotmail.com>
2024-12-01 20:07:10 +0100

    slice it up

--- src/2024/day1.zig
+++ src/2024/day1.zig
@@ -2,42 +2,60 @@ const std = @import("std");
 const print = std.debug.print;
 const input = @embedFile("day1.txt");
 
+pub fn part1(left: []i32, right: []i32) void {
+    std.mem.sort(i32, left, {}, std.sort.asc(i32));
+    std.mem.sort(i32, right, {}, std.sort.asc(i32));
+
+    var sum: u32 = 0;
+    for (0..left.len) |i| {
+        sum += @abs(left[i] - right[i]);
+    }
+
+    print("part1: {d}\n", .{sum});
+}
+
+pub fn part2(left: []i32, h: *std.AutoHashMap(i32, i32)) void {
+    var sum: i32 = 0;
+    for (left) |i| {
+        const count = h.get(i) orelse 0;
+        sum += i * count;
+    }
+    print("part2: {d}\n", .{sum});
+}
+
+pub fn part2Mem(left: []i32, right: []i32) void {
+    var sum: i32 = 0;
+    for (left) |i| {
+        const count: i32 = @intCast(std.mem.count(i32, right, &[_]i32{i}));
+        sum += count * i;
+    }
+    print("part2 mem: {d}\n", .{sum});
+}
+
 pub fn solve() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-
     var h = std.AutoHashMap(i32, i32).init(allocator);
     defer h.deinit();
 
-    var a: [1000]i32 = undefined;
-    var b: [1000]i32 = undefined;
-
     var it = std.mem.tokenizeScalar(u8, input, '\n');
-    var idx: usize = 0;
+
+    var a: std.ArrayList(i32) = std.ArrayList(i32).init(allocator);
+    var b: std.ArrayList(i32) = std.ArrayList(i32).init(allocator);
 
     while (it.next()) |token| {
         var line = std.mem.split(u8, token, "   ");
-        const left = line.next().?;
-        const right = line.next().?;
-        const left_value = try std.fmt.parseInt(i32, left, 10);
-        const right_value = try std.fmt.parseInt(i32, right, 10);
-        a[idx] = (left_value);
-        b[idx] = (right_value);
+        const left_value = try std.fmt.parseInt(i32, line.next().?, 10);
+        const right_value = try std.fmt.parseInt(i32, line.next().?, 10);
+        try a.append(left_value);
+        try b.append(right_value);
         if (h.get(right_value)) |v| try h.put(right_value, v + 1) else try h.put(right_value, 1);
-        idx += 1;
     }
 
-    std.mem.sort(i32, &a, {}, std.sort.asc(i32));
-    std.mem.sort(i32, &b, {}, std.sort.asc(i32));
+    const left = try a.toOwnedSlice();
+    const right = try b.toOwnedSlice();
 
-    const va: @Vector(1000, i32) = a;
-    const vb: @Vector(1000, i32) = b;
-    const c = @abs(va - vb);
-    var d: i32 = 0;
-    print("hello advent of code {any}\n", .{@reduce(.Add, c)});
-    for (a) |i| {
-        const count = h.get(i) orelse 0;
-        d += i * count;
-    }
-    print("{any}", .{d});
+    part1(left, right);
+    part2(left, &h);
+    part2Mem(left, right);
 }
