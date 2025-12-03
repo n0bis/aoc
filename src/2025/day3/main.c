8e9a95d02c158581259bb8166486d31f8e3d6d8c
n0bis <supermads@hotmail.com>
2025-12-03 21:13:34 +0100

    day3 part2

--- src/2025/day3/main.c
+++ src/2025/day3/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
-  FILE* file = fopen("input.txt", "r");
+  FILE* file = fopen("day3.txt", "r");
 
   if (file == NULL) {
     perror("Error opening file");
@@ -10,29 +11,33 @@ int main() {
   }
 
   char line[128];
-  int joltage = 0;
+  long long joltage = 0;
   while (fgets(line, sizeof(line), file) != NULL) {
-    int max = -1;
+    line[strcspn(line, "\n")] = '\0';
     int len = strlen(line);
-    int right_max[128] = {0};
-    right_max[len - 1] = -1;
-    for (int i = len - 2; i >= 0; i--) {
-      int digit = line[i + 1] - '0';
-      right_max[i] = digit > right_max[i + 1] ? digit : right_max[i + 1];
-    }
-    for (int i = 0; i < len - 1; i++) {
-      int first = line[i] - '0';
-      int second = right_max[i];
-      if (second >= 0) {
-        int val = first * 10 + second;
-        if (val > max) max = val;
-      }
-    }
+    int digits = 12;
+    int start = 0;
+    char numstr[32];
 
-    joltage += max;
+    for (int i = 0; i < digits; i++) {
+      int max = -1;
+      int max_pos = -1;
+      int end = len - (digits - i);
+      for (int j = start; j <= end; j++) {
+        int digit = line[j] - '0';
+        if (digit > max) {
+          max = digit;
+          max_pos = j;
+        }
+      }
+      start = max_pos + 1;
+      numstr[i] = '0' + max;
+    }
+    numstr[digits] = '\0';
+    joltage += atoll(numstr);
   }
   
-  printf("Total joltage: %d\n", joltage); // expected 17359
+  printf("Total joltage: %lld\n", joltage);
   fclose(file);
   return 0;
 }
